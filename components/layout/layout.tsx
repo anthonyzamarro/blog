29556a938a34ffe20d5cde6b49277109054ac01c
Anthony Zamarro <azamarro@veracode.com>
2022-01-29 14:10:43 -0500

    update files to use typescript

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,13 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from './layout.module.css'
-import utilStyles from '../../styles/utils.module.css'
+import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Anthony'
+const name = '[Your Name]'
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+export default function Layout({
+    children,
+    home
+}: {
+    children: React.ReactNode
+    home?: boolean
+}) {
     return (
         <div className={styles.container}>
             <Head>
--- lib/posts.ts
+++ lib/posts.ts
@@ -20,11 +20,10 @@ export function getSortedPostsData() {
         // Use gray-matter to parse the post metadata section
         const matterResult = matter(fileContents)
 
-
         // Combine the data with the id
         return {
             id,
-            ...matterResult.data
+            ...(matterResult.data as { date: string; title: string })
         }
     })
     // Sort posts by date
@@ -48,7 +47,7 @@ export function getAllPostIds() {
     })
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
     const fullPath = path.join(postsDirectory, `${id}.md`)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
 
@@ -65,6 +64,6 @@ export async function getPostData(id) {
     return {
         id,
         contentHtml,
-        ...matterResult.data
+        ...(matterResult.data as { date: string; title: string })
     }
 }
